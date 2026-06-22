ee860301df572d2a6c67467e5c75d2d91b248f4d
cobroux <115625830+cobroux@users.noreply.github.com>
2026-06-22 10:24:22 +0200

    :wrench: Adding init spring files

---
+++ src/main/java/io/oltre_backend/OltreApplication.java
@@ -0,0 +1,13 @@
+package io.oltre_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OltreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OltreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/oltre_backend/OltreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.oltre_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OltreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
