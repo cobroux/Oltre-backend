31707e1c1244c68306b8358fec89ad570bc81d4d
cobroux <115625830+cobroux@users.noreply.github.com>
2026-06-26 11:08:06 +0200

    test flyway

---
+++ src/main/resources/db/migration/V2__data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id, username, age, birth_date)
+VALUES (1, 'Admin', 30, '1996-05-12')
+ON DUPLICATE KEY UPDATE username=username;
+
+INSERT INTO expenses (id, expenses_name, amount, rec_type, start_date, user_id)
+VALUES (1, 'Netflix', 13, 'Monthly', NOW(), 1),
+       (2, 'Spotify', 10, 'Monthly', NOW(), 1)
+ON DUPLICATE KEY UPDATE expenses_name=expenses_name;
